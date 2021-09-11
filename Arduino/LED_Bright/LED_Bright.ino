cc1f1f881328158a94c3437a1bac2e210464f568
גל אפל <galapple96@gmail.com>
2021-09-11 13:05:34 +0300

    nit changes

--- Arduino/LED_Bright/LED_Bright.ino
+++ Arduino/LED_Bright/LED_Bright.ino
@@ -1,11 +1,15 @@
-int buttonPin1=12;
-int buttonPin2=11;
+// Project goal is to change the brightness of LED,by two buttons. 
+// buzzer indicates reaching maximum value.
+
+int buttonPin1 = 12;
+int buttonPin2 = 11;
 int buttonVal1;
 int buttonVal2;
 int dt = 250;
 int LEDbright = 0;
-int LEDPin=3;
+int LEDPin = 3;
 int buzzPin = 2;
+
 void setup() {
  Serial.begin(9600);
  pinMode(buttonPin1, INPUT);
@@ -16,31 +20,30 @@ void setup() {
 }
 
 void loop() {
-buttonVal1=digitalRead(buttonPin1);
-buttonVal2=digitalRead(buttonPin2);
-Serial.println("Button 1 = ");
-Serial.println(buttonVal1);
-Serial.println("Button 2 = ");
-Serial.println(buttonVal2);
-delay(dt);
-if (buttonVal1==0){
-  LEDbright=LEDbright+5;
-}
-if (buttonVal2==0){
-  LEDbright=LEDbright-5;
-}
-if (LEDbright>255){
-  LEDbright=255;
-  digitalWrite(buzzPin, HIGH);
+  buttonVal1 = digitalRead(buttonPin1);
+  buttonVal2 = digitalRead(buttonPin2);
+  Serial.println("Button 1 = ");
+  Serial.println(buttonVal1);
+  Serial.println("Button 2 = ");
+  Serial.println(buttonVal2);
   delay(dt);
-  digitalWrite(buzzPin, LOW);
-}
-if (LEDbright<0){
-  LEDbright=0;
-   digitalWrite(buzzPin, HIGH);
-   delay(dt);
-   digitalWrite(buzzPin, LOW);
-}
-
-analogWrite(LEDPin, LEDbright);
+  if (buttonVal1 == 0){
+    LEDbright = LEDbright + 5;
+  }
+  if (buttonVal2 == 0){
+    LEDbright = LEDbright - 5;
+  }
+  if (LEDbright > 255){
+    LEDbright = 255;
+    digitalWrite(buzzPin, HIGH);
+    delay(dt);
+    digitalWrite(buzzPin, LOW);
+  }
+  if (LEDbright < 0){
+    LEDbright = 0;
+     digitalWrite(buzzPin, HIGH);
+     delay(dt);
+     digitalWrite(buzzPin, LOW);
+  }
+  analogWrite(LEDPin, LEDbright);
 }
